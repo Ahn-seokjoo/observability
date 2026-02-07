4efb1c1ad3a95a1913cc4842666e4c613c70691a
Ahn-seokjoo <tjrwn0716@naver.com>
2026-02-08 01:47:39 +0900

    Loki 보이도록 수정

--- src/main/kotlin/com/sample/observability/HelloController.kt
+++ src/main/kotlin/com/sample/observability/HelloController.kt
@@ -1,13 +1,17 @@
 package com.sample.observability
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
 
+    private val loggger = LoggerFactory.getLogger(HelloController::class.java)
+
     @GetMapping("/hello")
     fun hello(): String {
+        loggger.info("hi called")
         Thread.sleep(1000)
         return "Hello Observe!"
     }
