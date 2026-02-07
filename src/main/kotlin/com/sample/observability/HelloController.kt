9d3ebc1f2008015c6b5b96d5ce1a245a993c4104
Ahn-seokjoo <tjrwn0716@naver.com>
2026-02-08 01:55:24 +0900

    여러 상황 테스트

--- src/main/kotlin/com/sample/observability/HelloController.kt
+++ src/main/kotlin/com/sample/observability/HelloController.kt
@@ -3,6 +3,7 @@ package com.sample.observability
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.math.sqrt
 
 @RestController
 class HelloController {
@@ -14,5 +15,42 @@ class HelloController {
         loggger.info("hi called")
         Thread.sleep(1000)
         return "Hello Observe!"
+    }
+
+    @GetMapping("/cpu")
+    fun cpu(): String {
+        val start = System.currentTimeMillis()
+
+        while (System.currentTimeMillis() - start < 5000) {
+            sqrt(Math.random()) // 의미없는 연산
+        }
+
+        return "CPU Burned"
+    }
+
+    @GetMapping("/memory")
+    fun memory(): String {
+        val list = mutableListOf<ByteArray>()
+
+        repeat(1000) {
+            list.add(ByteArray(10 * 1024 * 1024)) // 10MB
+            Thread.sleep(100)
+        }
+
+        return "Memory Loaded"
+    }
+
+    @GetMapping("/gc")
+    fun gc(): String {
+        repeat(5_000_000) {
+            val tmp = "GC-" + it + Math.random()
+        }
+        return "GC Triggered"
+    }
+
+    @GetMapping("/thread")
+    fun thread(): String {
+        Thread.sleep(30_000) // 30초 블로킹
+        return "Thread Blocked"
     }
 }
